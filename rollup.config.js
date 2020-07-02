c38c8933383a912bc6230d9af0826669d80a7b59
Joe Becher <jwbecher@drazisil.com>
2020-07-02 11:06:58 -0400

    Switch to nexe

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default {
+    input: "bin/codecov",
+    output: {
+        file: "dist/codecov.js",
+        format: "cjs"
+    },
+    plugins: [commonjs(), resolve()]
+};
