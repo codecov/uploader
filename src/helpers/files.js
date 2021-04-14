d04bfec0e732e1e9d8f105ecff6750ec9aa22df5
Joe Becher <71270647+drazisil-codecov@users.noreply.github.com>
2021-04-14 16:25:15 -0400

    chore: replace eslint with standard and lint (#49)
    * chore: replace eslint with standard and lint

--- src/helpers/files.js
+++ src/helpers/files.js
@@ -1,181 +1,180 @@
-const child_process = require("child_process");
-const fs = require("fs");
-const path = require("path");
-const glob = require("glob");
+const childProcess = require('child_process')
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
 
-async function getFileListing(projectRoot) {
-  return getAllFiles(projectRoot, projectRoot).join("");
+async function getFileListing (projectRoot) {
+  return getAllFiles(projectRoot, projectRoot).join('')
 }
 
-function manualBlacklist() {
+function manualBlacklist () {
   // TODO: honor the .gitignore file instead of a hard-coded list
   return [
-    "node_modules",
-    ".git",
-    ".nyc_output",
-    ".circleci",
-    ".nvmrc",
-    ".gitignore"
-  ];
+    'node_modules',
+    '.git',
+    '.nyc_output',
+    '.circleci',
+    '.nvmrc',
+    '.gitignore'
+  ]
 }
 
-function globBlacklist() {
+function globBlacklist () {
   // TODO: honor the .gitignore file instead of a hard-coded list
   return [
-    "node_modules/**/*",
-    ".git",
-    ".nyc_output",
-    ".circleci",
-    ".nvmrc",
-    ".gitignore"
-  ];
+    'node_modules/**/*',
+    '.git',
+    '.nyc_output',
+    '.circleci',
+    '.nvmrc',
+    '.gitignore'
+  ]
 }
 
-function coverageFilePatterns() {
+function coverageFilePatterns () {
   return [
-    "*coverage*.*",
-    "nosetests.xml",
-    "jacoco*.xml",
-    "clover.xml",
-    "report.xml",
-    "*.codecov.*",
-    "codecov.*",
-    "cobertura.xml",
-    "excoveralls.json",
-    "luacov.report.out",
-    "coverage-final.json",
-    "naxsi.info",
-    "lcov.info",
-    "lcov.dat",
-    "*.lcov",
-    "*.clover",
-    "cover.out",
-    "gcov.info",
-    "*.gcov",
-    "*.lst"
-  ];
+    '*coverage*.*',
+    'nosetests.xml',
+    'jacoco*.xml',
+    'clover.xml',
+    'report.xml',
+    '*.codecov.*',
+    'codecov.*',
+    'cobertura.xml',
+    'excoveralls.json',
+    'luacov.report.out',
+    'coverage-final.json',
+    'naxsi.info',
+    'lcov.info',
+    'lcov.dat',
+    '*.lcov',
+    '*.clover',
+    'cover.out',
+    'gcov.info',
+    '*.gcov',
+    '*.lst'
+  ]
 }
 
-function getCoverageFiles(projectRoot, coverageFilePatterns) {
-  let files = [];
+function getCoverageFiles (projectRoot, coverageFilePatterns) {
+  let files = []
   for (let index = 0; index < coverageFilePatterns.length; index++) {
-    const pattern = coverageFilePatterns[index];
+    const pattern = coverageFilePatterns[index]
     const newFiles = glob.sync(`**/${pattern}`, {
       cwd: projectRoot,
       ignore: globBlacklist()
-    });
+    })
 
-    files = files.concat(newFiles);
+    files = files.concat(newFiles)
   }
-  return files;
+  return files
 }
 
-function isBlacklisted(projectRoot, file, manualBlacklist) {
-  const blacklist = manualBlacklist;
-  return blacklist.includes(file);
+function isBlacklisted (projectRoot, file, manualBlacklist) {
+  const blacklist = manualBlacklist
+  return blacklist.includes(file)
 }
 
-function fetchGitRoot() {
+function fetchGitRoot () {
   try {
     return (
-      child_process
-        .spawnSync("git", ["rev-parse", "--show-toplevel"])
+      childProcess
+        .spawnSync('git', ['rev-parse', '--show-toplevel'])
         .stdout.toString()
         .trimRight() ||
-      child_process
-        .spawnSync("hg", ["root"])
+      childProcess
+        .spawnSync('hg', ['root'])
         .stdout.toString()
         .trimRight() ||
       process.cwd()
-    );
+    )
   } catch (error) {
-    throw new Error("Error fetching git root. Please try using the -R flag.");
+    throw new Error('Error fetching git root. Please try using the -R flag.')
   }
 }
 
-function parseGitIgnore(projectRoot) {
-  const gitIgnorePath = path.join(projectRoot, ".gitignore");
-  let lines;
+function parseGitIgnore (projectRoot) {
+  const gitIgnorePath = path.join(projectRoot, '.gitignore')
+  let lines
   try {
-    lines = readAllLines(gitIgnorePath) || [];
+    lines = readAllLines(gitIgnorePath) || []
   } catch (error) {
-    throw new Error(`Unable to open ${gitIgnorePath}: ${error}`);
+    throw new Error(`Unable to open ${gitIgnorePath}: ${error}`)
   }
 
   const filteredLines = lines.filter(line => {
-    if (line === "" || line.startsWith("#")) {
-      return false;
+    if (line === '' || line.startsWith('#')) {
+      return false
     }
-    return true;
-  });
-  return filteredLines;
+    return true
+  })
+  return filteredLines
 }
 
-function getAllFiles(projectRoot, dirPath, arrayOfFiles) {
-  const files = fs.readdirSync(dirPath);
-  arrayOfFiles = arrayOfFiles || [];
+function getAllFiles (projectRoot, dirPath, arrayOfFiles) {
+  const files = fs.readdirSync(dirPath)
+  arrayOfFiles = arrayOfFiles || []
 
-  files.forEach(function(file) {
+  files.forEach(function (file) {
     if (
       fs.statSync(path.join(dirPath, file)).isDirectory() &&
       !isBlacklisted(projectRoot, file, manualBlacklist())
     ) {
       arrayOfFiles = getAllFiles(
         projectRoot,
-        path.join(dirPath,  file),
+        path.join(dirPath, file),
         arrayOfFiles
-      );
+      )
     } else {
       if (!isBlacklisted(projectRoot, file, manualBlacklist())) {
         arrayOfFiles.push(
-          `${path.join(dirPath.replace(projectRoot, "."), file)}\n`
-        );
+          `${path.join(dirPath.replace(projectRoot, '.'), file)}\n`
+        )
       }
     }
-  });
+  })
 
-  return arrayOfFiles;
+  return arrayOfFiles
 }
 
-function readAllLines(filePath) {
-  const fileContents = fs.readFileSync(filePath);
+function readAllLines (filePath) {
+  const fileContents = fs.readFileSync(filePath)
 
-  const lines = fileContents.toString().split("\n") || [];
-  return lines;
+  const lines = fileContents.toString().split('\n') || []
+  return lines
 }
 
-function readCoverageFile(projectRoot, filePath) {
+function readCoverageFile (projectRoot, filePath) {
   try {
-    return fs.readFileSync(getFilePath(projectRoot, filePath));
+    return fs.readFileSync(getFilePath(projectRoot, filePath))
   } catch (error) {
-    throw new Error(`There was an error reading the coverage file: ${error}`);
+    throw new Error(`There was an error reading the coverage file: ${error}`)
   }
 }
 
-function endNetworkMarker() {
-  return "<<<<<< network\n";
+function endNetworkMarker () {
+  return '<<<<<< network\n'
 }
 
-function endFileMarker() {
-  return "<<<<<< EOF\n";
+function endFileMarker () {
+  return '<<<<<< EOF\n'
 }
 
-function fileHeader(filePath) {
-  return `# path=${filePath}\n`;
+function fileHeader (filePath) {
+  return `# path=${filePath}\n`
 }
 
-function getFilePath(projectRoot, filePath) {
-  if (filePath.startsWith("./")
-      || filePath.startsWith("/")
-      || filePath.startsWith(".\\")
-      || filePath.startsWith(".\\")) {
+function getFilePath (projectRoot, filePath) {
+  if (filePath.startsWith('./') ||
+      filePath.startsWith('/') ||
+      filePath.startsWith('.\\') ||
+      filePath.startsWith('.\\')) {
     return filePath
   }
-  if (projectRoot === ".") {
-    return path.join(".", filePath)
+  if (projectRoot === '.') {
+    return path.join('.', filePath)
   }
   return path.join(projectRoot, filePath)
-
 }
 
 module.exports = {
@@ -189,4 +188,4 @@ module.exports = {
   getCoverageFiles,
   coverageFilePatterns,
   getFilePath
-};
+}
--- test/helpers/validate.test.js
+++ test/helpers/validate.test.js
@@ -1,30 +1,30 @@
-const validate = require("../../src/helpers/validate");
+const validate = require('../../src/helpers/validate')
 
-describe("Input Validators", function() {
-  describe("Tokens", function() {
-    it("Returns true with a valid token", function() {
-      expect(validate.validateToken("1bc123")).toBe(true);
-    });
-    it("Returns false with an invalid token", function() {
-      expect(validate.validateToken("1bc1 23")).toBe(false);
-    });
-  });
+describe('Input Validators', function () {
+  describe('Tokens', function () {
+    it('Returns true with a valid token', function () {
+      expect(validate.validateToken('1bc123')).toBe(true)
+    })
+    it('Returns false with an invalid token', function () {
+      expect(validate.validateToken('1bc1 23')).toBe(false)
+    })
+  })
 
-  describe("Flags", function() {
-    it("Should fail with a dash", function() {
-      expect(validate.validateFlags("moo-foor")).toBe(false);
-    });
-  });
+  describe('Flags', function () {
+    it('Should fail with a dash', function () {
+      expect(validate.validateFlags('moo-foor')).toBe(false)
+    })
+  })
 
-  describe("URLs", function() {
-    it("Returns true with a valid URL", function() {
-      expect(validate.validateURL("https://codecov.io")).toBe(true);
-    });
-    it("Returns false with an invalid URL", function() {
-      expect(validate.validateURL("not.a.URL.com")).toBe(false);
-    });
-    it("Returns false with an empty URL", function() {
-      expect(validate.validateURL("")).toBe(false);
-    });
-  });
-});
+  describe('URLs', function () {
+    it('Returns true with a valid URL', function () {
+      expect(validate.validateURL('https://codecov.io')).toBe(true)
+    })
+    it('Returns false with an invalid URL', function () {
+      expect(validate.validateURL('not.a.URL.com')).toBe(false)
+    })
+    it('Returns false with an empty URL', function () {
+      expect(validate.validateURL('')).toBe(false)
+    })
+  })
+})
