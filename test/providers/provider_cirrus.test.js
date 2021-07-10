cd95eec82703290d3db8b4593f80d901c4f08ffb
Ed Moore <eddie.moore@gmail.com>
2021-07-10 23:34:50 +1000

    Adding more tests to cirrus CI

--- test/providers/provider_cirrus.test.js
+++ test/providers/provider_cirrus.test.js
@@ -56,4 +56,38 @@ describe('Cirrus Params', () => {
     const params = providerCirrus.getServiceParams(inputs)
     expect(params).toMatchObject(expected)
   })
+
+  it('gets correct params for overrides', () => {
+    const inputs = {
+      args: {
+        branch: 'branch',
+        build: 3,
+        pr: '2',
+        sha: 'testsha',
+        slug: 'testOrg/testRepo',
+      },
+      envs: {
+        CI: true,
+        CIRRUS_CI: true,
+        CIRRUS_BRANCH: 'master',
+        CIRRUS_CHANGE_IN_REPO: 'testingsha',
+        CIRRUS_BUILD_ID: 2,
+        CIRRUS_PR: 1,
+        CIRRUS_REPO_FULL_NAME: 'https:/example.com/repo',
+      },
+    }
+    const expected = {
+      branch: 'branch',
+      build: 3,
+      buildURL: '',
+      commit: 'testsha',
+      job: '',
+      pr: '2',
+      service: 'cirrus-ci',
+      slug: 'testOrg/testRepo',
+    }
+
+    const params = providerCirrus.getServiceParams(inputs)
+    expect(params).toMatchObject(expected)
+  })
 })
