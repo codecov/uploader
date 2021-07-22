1cbd30d02655ea85afe47edf34fc2f3f5b4fe053
Joe Becher <71270647+drazisil-codecov@users.noreply.github.com>
2021-07-22 14:59:55 -0400

    Update src/helpers/token.js
    Co-authored-by: RA80533 <32469082+RA80533@users.noreply.github.com>

--- src/helpers/token.js
+++ src/helpers/token.js
@@ -8,7 +8,7 @@ const validateHelpers = require('./validate')
 /**
  *
  * @param {object} inputs
- * @param {strinh} projectRoot
+ * @param {string} projectRoot
  * @returns string
  */
 function getToken(inputs, projectRoot) {
