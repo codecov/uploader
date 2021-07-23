5a5300ca3f4fb5c399e18f46314102a7ae607bd2
RA80533 <32469082+RA80533@users.noreply.github.com>
2021-07-23 11:58:40 -0400

    Add `await`

--- src/index.js
+++ src/index.js
@@ -114,7 +114,7 @@ async function main(args) {
   let coverageFilePaths = []
   if (!args.file) {
     info('Searching for coverage files...')
-    coverageFilePaths = fileHelpers.getCoverageFiles(
+    coverageFilePaths = await fileHelpers.getCoverageFiles(
       args.dir || projectRoot,
       // TODO: Determine why this is so slow (I suspect it's walking paths it should not)
       fileHelpers.coverageFilePatterns(),
