63c0a66cbbef34c6581d21c7f13949f56ec8e2c0
Joe Becher <joe@codecov.io>
2021-09-13 11:18:02 -0400

    chore: update @types/jest, jest, ts-jest to version 27

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,5 @@ module.exports = {
   coverageReporters: ['text', 'cobertura', 'html'],
   setupFilesAfterEnv: ['<rootDir>/test/test_helpers.js'],
   reporters: ['jest-spec-reporter'],
+  testPathIgnorePatterns: ['<rootDir>/dist/'],
 }
