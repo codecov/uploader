6fda56ac5d60ec10640227c2076d1edbdacfc457
Mitchell Borrego <mitchell@codecov.io>
2021-10-01 10:02:22 -0400

    Update provider_drone.test.ts

--- test/providers/provider_drone.test.ts
+++ test/providers/provider_drone.test.ts
@@ -47,7 +47,7 @@ describe('Drone Params', () => {
         DRONE_BUILD_NUMBER: '2',
         DRONE_PULL_REQUEST: '1',
         DRONE_BUILD_URL: 'https://www.drone.io/',
-        DRONE_REPO_LINK: 'https:/example.com/repo',
+        DRONE_REPO: 'repo',
       },
     }
     const expected: IServiceParams = {
@@ -58,7 +58,7 @@ describe('Drone Params', () => {
       job: '',
       pr: '1',
       service: 'drone',
-      slug: 'https:/example.com/repo',
+      slug: 'repo',
     }
     const params = providerDrone.getServiceParams(inputs)
     expect(params).toMatchObject(expected)
