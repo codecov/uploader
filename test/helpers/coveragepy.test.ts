de90c56614de8eea5d46aee25f106598cccd534d
Tom Hu <tom@codecov.io>
2022-04-13 14:33:00 -0400

    fix: add tests for coveragepy

---
+++ test/helpers/coveragepy.test.ts
@@ -0,0 +1,30 @@
+import td from 'testdouble'
+import childProcess from 'child_process'
+import { generateCoveragePyFile } from '../../src/helpers/coveragepy'
+
+describe('generateCoveragePyFile()', () => {
+    afterEach(() => {
+        td.reset()
+    })
+
+    it('should run when coveragepy is asked for', async () => {
+        const spawnSync = td.replace(childProcess, 'spawnSync')
+        td.when(spawnSync('coverage')).thenReturn({
+            stdout: 'coverage installed',
+            error: null
+        })
+        td.when(spawnSync('coverage', td.matchers.contains('xml'))).thenReturn({
+            stdout: 'xml',
+            error: null
+        })
+
+        expect(await generateCoveragePyFile()).toBe('xml')
+    })
+
+    it('should return an error when coveragepy is not installed', async () => {
+        const spawnSync = td.replace(childProcess, 'spawnSync')
+        td.when(spawnSync('coverage')).thenReturn({ error: "command not found: coverage" })
+
+        await expect(generateCoveragePyFile()).rejects.toThrowError(/coveragepy is not installed/)
+    })
+})
