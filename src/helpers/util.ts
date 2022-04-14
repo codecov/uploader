a758b905f41d47c4ff453312384943dbd0b1ff5e
Tom Hu <tom@codecov.io>
2022-04-14 15:18:02 -0400

    fix: increase spawnsync buffer

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -1,5 +1,7 @@
 import childprocess from 'child_process'
 
+const SpawnProcessBufferSize = 10485760 // 10MiB
+
 export function isProgramInstalled(programName: string): boolean {
   return !childprocess.spawnSync(programName).error
 }
@@ -8,7 +10,11 @@ export function runExternalProgram(
   programName: string,
   optionalArguments: string[] = [],
 ): string {
-  const result = childprocess.spawnSync(programName, optionalArguments)
+  const result = childprocess.spawnSync(
+    programName,
+    optionalArguments,
+    {maxBuffer: SpawnProcessBufferSize}
+  )
   if (result.error) {
     throw new Error(`Error running external program: ${result.error}`)
   }
