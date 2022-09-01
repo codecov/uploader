29b240c07ca4bfc0312c66eb4d2fd78f2bf6c0db
Tom Hu <88201630+thomasrockhu-codecov@users.noreply.github.com>
2022-09-01 11:01:04 -0400

    feat: create fixes on c-like and php files (#869)
    * feat: create fixes on c-like and php files
    
    * fix: add e2e test for fixes

---
+++ test/fixtures/fixes/example.php
@@ -0,0 +1,20 @@
+<?php namespace Example;
+
+class Example
+{
+    public static function go()
+    {
+        $test_array = [
+          1,
+          2,
+          3,
+        ]
+
+        if (false) {
+            return true;
+        }
+
+        return false;
+
+    }
+}
