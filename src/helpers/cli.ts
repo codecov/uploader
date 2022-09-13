74b1e863f840584157a3f4b2a190ae89566e7be8
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2022-09-13 15:25:00 +0200

    fix(yargs): specify types for every CLI option (#878)
    Fixes https://github.com/codecov/uploader/issues/876

--- src/helpers/cli.ts
+++ src/helpers/cli.ts
@@ -10,11 +10,13 @@ const args: ICLIArgument[] = [
   {
     alias: 'B',
     name: 'branch',
+    type: 'string',
     description: 'Specify the branch manually',
   },
   {
     alias: 'b',
     name: 'build',
+    type: 'number',
     description: 'Specify the build number manually',
   },
   {
@@ -27,11 +29,14 @@ const args: ICLIArgument[] = [
   {
     alias: 'C',
     name: 'sha',
+    type: 'string',
     description: 'Specify the commit SHA manually',
   },
   {
     alias: 'CL',
     name: 'changelog',
+    type: 'boolean',
+    default: false,
     description: 'Display a link for the current changelog'
   },
   {
@@ -49,11 +54,13 @@ const args: ICLIArgument[] = [
   {
     alias: 'f',
     name: 'file',
+    type: 'string',
     description: 'Target file(s) to upload',
   },
   {
     alias: 'F',
     name: 'flags',
+    type: 'string',
     default: '',
     description: 'Flag the upload to group coverage metrics',
   },
@@ -103,17 +110,20 @@ const args: ICLIArgument[] = [
   {
     alias: 'n',
     name: 'name',
+    type: 'string',
     default: '',
     description: 'Custom defined name of the upload. Visible in Codecov UI',
   },
   {
     alias: 'N',
     name: 'parent',
+    type: 'string',
     description: "The commit SHA of the parent for which you are uploading coverage. If not present, the parent will be determined using the API of your repository provider. When using the repository provider's API, the parent is determined via finding the closest ancestor to the commit.",
   },
   {
     alias: 'P',
     name: 'pr',
+    type: 'number',
     description: 'Specify the pull request number manually',
   },
   {
@@ -139,17 +149,20 @@ const args: ICLIArgument[] = [
   {
     alias: 's',
     name: 'dir',
+    type: 'string',
     description: 'Directory to search for coverage reports.\nAlready searches project root and current working directory',
   },
   {
     alias: 'T',
     name: 'tag',
+    type: 'string',
     default: '',
     description: 'Specify the git tag',
   },
   {
     alias: 't',
     name: 'token',
+    type: 'string',
     default: '',
     description: 'Codecov upload token',
   },
